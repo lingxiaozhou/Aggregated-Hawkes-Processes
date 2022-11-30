d0ae8d60722deb7b68502690673e1c3d8512659f
lingxiaozhou <35700851+lingxiaozhou@users.noreply.github.com>
2022-11-30 16:07:10 -0500

    minor

---
+++ code/32_sim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=gpapadogeorgou
+#SBATCH --qos=gpapadogeorgou-b
+#SBATCH --mem=6G
+#SBATCH --array=1-400
+#SBATCH --time=24:00:00               # Time limit hrs:min:sec
+
+module load R/3.6
+R CMD BATCH --no-save "--args ${agg1} ${agg2} ${SLURM_ARRAY_TASK_ID}" Output/3_sims/1_agg${agg1}/2_agg${agg2}/Out_files/Routput_${SLURM_ARRAY_TASK_ID}.Rout
+
+
